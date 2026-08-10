04063efec5dd53a10235ce35e416b572a6ade415
nick.allan <nick.allan@kerv.com>
2026-08-10 18:01:53 +0100

    Clean repository artifacts and add .NET ignore rules

--- Query2Excel.App/bin/Debug/net8.0/Scripts/Example.sql
+++
@@ -1,7 +0,0 @@
-SELECT __SheetName = 'Objects',
-    __Title = 'List of Objects',
-    __Description = 'This query returns a list of all objects in the database, including tables, views, and stored procedures.';
-EXEC sp_find;
-
-SELECT __Description = 'This query returns a list of currently active users and their processes.';
-EXEC sp_who2;
--- Query2Excel.Tests/bin/Debug/net8.0/Scripts/Example.sql
+++
@@ -1,5 +0,0 @@
-SELECT __SheetName = 'Objects',
-    __Title = 'List of Objects',
-    __Description = 'This query returns a list of all objects in the database, including tables, views, and stored procedures.';
-EXEC sp_find;
-EXEC sp_who2;
