4add46f9af0ebb0261b536d9c57d671266008da9
nick.allan <nick.allan@kerv.com>
2026-08-11 17:32:35 +0100

    chore: update Query2Excel.App/Abstractions/IQuery2ExcelService.cs

--- Query2Excel.App/Abstractions/IQuery2ExcelService.cs
+++
@@ -1,8 +0,0 @@
-using Query2Excel.App.Models;
-
-namespace Query2Excel.App.Abstractions;
-
-public interface IQuery2ExcelService
-{
-    Task RunAsync(Query2ExcelRequest request, CancellationToken cancellationToken);
-}
