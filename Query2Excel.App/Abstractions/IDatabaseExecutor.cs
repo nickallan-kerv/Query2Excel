a594e2165d1c0fc6e5c5163a3caf61f92b136fcf
nick.allan <nick.allan@kerv.com>
2026-08-11 17:32:34 +0100

    chore: update Query2Excel.App/Abstractions/IDatabaseExecutor.cs

--- Query2Excel.App/Abstractions/IDatabaseExecutor.cs
+++
@@ -1,8 +0,0 @@
-using Query2Excel.App.Models;
-
-namespace Query2Excel.App.Abstractions;
-
-public interface IDatabaseExecutor
-{
-    Task<QueryExecutionResult> ExecuteAsync(QueryExecutionRequest request, CancellationToken cancellationToken);
-}
