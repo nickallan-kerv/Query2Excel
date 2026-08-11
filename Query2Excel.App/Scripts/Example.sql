c29351f7071ee7d4f5699a17a5714f1df3ba23bb
nick.allan <nick.allan@kerv.com>
2026-08-11 17:32:39 +0100

    chore: update Query2Excel.App/Scripts/Example.sql

--- Query2Excel.App/Scripts/Example.sql
+++ Query2Excel.App/Scripts/Example.sql
@@ -3,5 +3,6 @@ SELECT __SheetName = 'Objects',
     __Description = 'This query returns a list of all objects in the database, including tables, views, and stored procedures.';
 EXEC sp_find;
 
-SELECT __Description = 'This query returns a list of currently active users and their processes.';
+SELECT __Description = 'This query returns a list of currently active users and their processes.',
+    __AppendBelowPreviousTable = 1;
 EXEC sp_who2;
