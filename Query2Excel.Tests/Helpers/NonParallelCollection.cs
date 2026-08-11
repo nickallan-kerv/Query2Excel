39445a26b81a33ac3709cb3d4e9a430252d76069
nick.allan <nick.allan@kerv.com>
2026-08-11 17:33:04 +0100

    chore: add Query2Excel.Tests/Helpers/NonParallelCollection.cs

---
+++ Query2Excel.Tests/Helpers/NonParallelCollection.cs
@@ -0,0 +1,6 @@
+namespace Query2Excel.Tests;
+
+[CollectionDefinition("NonParallel-WorkingDirectory", DisableParallelization = true)]
+public sealed class NonParallelWorkingDirectoryCollection
+{
+}
